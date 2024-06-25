efa660a9fecdf9d08ca6310f839e9684d24137b8
Paulo Mascarenhas <paulo.mascarenhas18@gmail.com>
2024-06-25 16:36:18 -0300

    Update README.py

--- README.py
+++ README.py
@@ -1,5 +1,5 @@
 # Sistema-Bancario-Basico-DIO-
-print(" ########## Ben-vindo ao banco Paulinho ###########")
+print(" ########## Ben-vindo ao banco Paulinho ##TESTE DE PULL do local pro remoto ###########")
 
 menu = """
 
